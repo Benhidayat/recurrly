cfbfcb933d438c380694d65a8b6ee008cba69ee9
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2026-06-04 12:32:51 +0700

    add: tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Link href='/onboarding' className="mt-4 rounded bg-primary text-white p-4">
+        Go to Onboarding
+      </Link>
     </View>
   );
 }
